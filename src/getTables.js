474fa6de4a6a380060e66479aa0ddf73aebde5b6
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-06-30 18:44:57 +0300

    fixed the output on error, get_array funciton

--- src/getTables.js
+++ src/getTables.js
@@ -2,19 +2,19 @@ import InputValidator from './InputValidator.js';
 import ArrayUtils from './arrayUtils.js';
 
 export default class Table {
-  constructor() {
+    constructor() {
         this.validator = new InputValidator();
         this.arrUtils = new ArrayUtils();
     }
 
     get_table(arr) {
         if (!this.validator.is2DArray(arr) || !this.validator.isValidArray(arr)) {
-        console.log(`invalid array: ${arr}`);
-        throw new Error('Invalid array');
+            console.log(`invalid array: ${arr}`);
+            throw new Error('Invalid array');
         }
 
         const cleanedArr = this.arrUtils.removeEmptyArrays(arr);
-        
+
         const headers = cleanedArr[0];
         const content = cleanedArr.slice(1);
 
@@ -23,23 +23,23 @@ export default class Table {
         }
 
         return content.map(row => {
-        let dict_row = {};
-        headers.forEach((header, i) => {
-            dict_row[header] = row[i];
-        });
-        return dict_row;
+            let dict_row = {};
+            headers.forEach((header, i) => {
+                dict_row[header] = row[i];
+            });
+            return dict_row;
         });
     }
 
     get_array(dict) {
-        if (!this.validator.isValidTable(dict)){
-            console.log(`Invalid Table: ${dict}`);
-            throw new Error ('invalid table')
+        if (!this.validator.isValidTable(dict)) {
+            console.log(`Invalid Table:`, dict);
+            throw new Error('invalid table')
         }
 
-        const headers = Object.keys(dict[0]); 
-        const content = dict.map(row=> Object.values(row)); 
-        
+        const headers = Object.keys(dict[0]);
+        const content = dict.map(row => Object.values(row));
+
         return [headers, ...content];
     }
 }
