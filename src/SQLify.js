438768dabadf109d95bb5497f2a6070a4052308d
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-06-30 18:43:48 +0300

    refactored SQLIFY module

--- src/SQLify.js
+++ src/SQLify.js
@@ -1,9 +1,48 @@
 import Table from "./getTables.js";
 import alasql from 'alasql';
-
+import InputValidator from "./InputValidator.js";
 export default class SQLify {
     constructor() {
-        this.tableModule = new Table();
-        this.alasqlInstance = alasql;
+        this.alasql = alasql;
+    }
+
+    load2DArrayAsTable(tableName, array2D){
+        const tableHelper = new Table();
+        const jsonData = tableHelper.get_table(array2D);
+        this.loadJsonAsTable(tableName, jsonData);
+    }
+
+    loadJsonAsTable(tableName, jsonData){
+        console.log(tableName, jsonData)
+        
+        const validator = new InputValidator();
+        if (!validator.isValidTable(jsonData)){
+            console.log(`the input:`, jsonData, `is not valid table.`)
+            throw new Error("Invalid Table")
+        }
+
+        const columns = Object.keys(jsonData[0]).map(col => `${col} TEXT`).join(', ');
+        const placeholders = Object.keys(jsonData[0]).map(() => '?').join(', ');
+        
+        this.alasql(`CREATE TABLE IF NOT EXISTS ${tableName} (${columns});`);
+        const insertStmt = this.alasql.compile(`INSERT INTO ${tableName} VALUES (${placeholders});`);
+        
+        jsonData.forEach(item => {
+          insertStmt(Object.values(item));
+        });
+    }
+    
+    execSQL(query, params) {
+        if (params) {
+            return alasql(query, params);
+        } else {
+            return alasql(query);
+        }
+    }
+
+    getGASfriendlyResults(results)
+    {
+        const tableHelper = new Table();
+        return tableHelper.get_array(results);
     }
 };
